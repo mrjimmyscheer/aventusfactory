63b0fd73d04ee04938ef2cb7aed4837ac490bb31
Thijs <thijsvanwaveren@outlook.com>
2025-03-01 16:03:30 +0100

    Added meta content

---
+++ Index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta content="description" content="Welkom op WeDoTech">
+    <meta name="keywords" content="WeDoTech, ICT, ICT opleiding">
+
+    <!-- Content die wordt laten zien als de website gedeeld word. -->
+    <meta property="og:title" content="WeDoTech - Meld je aan voor een leuke ICT opleiding">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://example.com/image.jpg">
+    <meta property="og:description" content="Een website voor een ICT opleiding">
+
+    <title>WeDoTech placeholder</title>
+</head>
+<body>
+
+<nav>
+
+</nav>
+
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
