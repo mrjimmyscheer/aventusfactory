af4e869f1b8b7944f4df658c8474a7750b720018
Thijs <thijsvanwaveren@outlook.com>
2025-03-13 18:58:47 +0100

    Added keywords, meta file and testing href links.

--- Index.php
+++ Index.php
@@ -1,3 +1,8 @@
+<?php
+include 'meta.php';
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -5,8 +10,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta content="description" content="Welkom op WeDoTech">
-    <meta name="keywords" content="WeDoTech, ICT, ICT opleiding">
+    <meta content="description" content="<?php echo $metaDescription; ?>">
+    <meta name="keywords" content="WeDoTech, WeTech, WeDo, ITC, CTI, ICT, ICT opleiding, IT, Software developer, Aventus, Aventus ICT, Aventus software developer, Software development Apeldoorn, ">
 
     <!-- Content die wordt laten zien als de website gedeeld word. -->
     <meta property="og:title" content="WeDoTech - Meld je aan voor een leuke ICT opleiding">
@@ -14,15 +19,27 @@
     <meta property="og:image" content="https://example.com/image.jpg">
     <meta property="og:description" content="Een website voor een ICT opleiding">
 
-    <title>WeDoTech placeholder</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title><?php echo $pageTitle; ?></title>
 </head>
 <body>
 
 <nav>
-    <img src="./assets/images/AventusLogo.png" alt="Aventus logo">
+    <!-- <img src="./assets/images/AventusLogo.png" alt="Aventus logo"> -->
 </nav>
+<?php
+// echo "hallo vanuit de Index.php";
+
+$page = $_GET['p'] ?? 'home';
+
+echo $page;
+// include "$page.php";
 
 
+?>
+<div class="border"><a href="http://localhost/wedotech/pages/home.php">home</a></div>
+<a href="http://localhost/wedotech/pages/huidigeStudenten.php">huidige studenten</a>
 
 <footer>
 
