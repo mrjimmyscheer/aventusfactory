d44082967b2b590efd9c565772bc21fb56dfdbba
Thijs <thijsvanwaveren@outlook.com>
2025-03-13 19:00:31 +0100

    Added meta title and description for home page.

---
+++ meta.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "";
+$metaDescription = "";
+
+
+/// Fallback title ///
+$pageTitle = "WeDoTech - De studie voor ICTers!";
+$metaDescription = "Bij WeDoTech leiden we creatieve studenten en programmeurs op doormiddel van code. ";
+?>
