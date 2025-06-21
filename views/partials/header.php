3b98879822f5149feaa8a54a38739b35daa56939
Thijs <thijsvanwaveren@outlook.com>
2025-06-21 15:45:06 +0200

    Added extra scripts & styling

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta content="description" content="<?php echo $metaDescription; ?>">
+    <meta name="description" content="<?php echo $metaDescription; ?>">
     <meta name="keywords" content="WeDoTech, ICT, ICT opleiding, IT, Software developer, Aventus, Aventus ICT, Aventus software developer, Software development Apeldoorn ">
 
     <!-- Content die wordt laten zien als de website gedeeld word. -->
@@ -12,9 +12,10 @@
     <meta property="og:type" content="website">
     <meta property="og:image" content="https://example.com/image.jpg">
     <meta property="og:description" content="Een website voor een ICT opleiding">
-
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
-     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="./scripts/menu.js"></script>
 
     <title><?php echo $pageTitle; ?></title>
 </head>
@@ -25,15 +26,21 @@
       <a href="<?php echo $baseLocal; ?>"><img src="assets/images/AventusLogo.png" alt="Aventus logo" style="width: 200px;"></a>
 
       <nav class="space-x-6 hidden md:flex">
-        <a href="#" class="text-gray-600 hover:text-blue-500">Studenten</a>
-        <a href="#" class="text-gray-600 hover:text-blue-500">Voor Bedrijven</a>
-        <a href="#" class="text-gray-600 hover:text-blue-500">Discord link</a>
+        <a href="<?php echo $baseLocal; ?>huidigeStudenten" class="text-gray-600 hover:text-blue-500">Studenten</a>
+        <a href="<?php echo $baseLocal; ?>stagebedrijven" class="text-gray-600 hover:text-blue-500">Voor Bedrijven</a>
+        <!-- <a href="#" class="text-gray-600 hover:text-blue-500">Discord link</a> -->
       </nav>
+
       <!-- Mobile Menu Icon -->
       <button class="md:hidden text-gray-600">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
         </svg>
       </button>
-    </div>
+
+      <ul class="content hidden">
+        <li><a href="<?php echo $baseLocal; ?>"></a>Home</li>
+        <li><a href="<?php echo $baseLocal; ?>huidigeStudenten">Studenten</a></li>
+        <li><a href="<?php echo $baseLocal; ?>stagebedrijven">Voor Bedrijven</a></li>
+      </ul>
   </header>
