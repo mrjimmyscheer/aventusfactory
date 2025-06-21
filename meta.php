7c1ae23aa2d0ebf51199b0e92464ace7253ffb5d
Thijs <thijsvanwaveren@outlook.com>
2025-06-21 15:35:06 +0200

    Made a dynamic meta file

--- meta.php
+++ meta.php
@@ -2,8 +2,21 @@
 $pageTitle = "";
 $metaDescription = "";
 
+if ($parts[1] == 'home') {
+    $pageTitle = "testing vanuit home";
+    $metaDescription = "Description home";
+} elseif ($parts[1] == 'stagebedrijven') {
+    $pageTitle = "testing vanuit stagebedrijven";
+    $metaDescription = "stagebedrijven description";
+} elseif ($parts[1] == 'huidigestudenten') {
+    $pageTitle = "Testing vanuit huidigestudenten";
+    $metaDescription = "Testing vanuit huidigestudenten";
+}
 
-/// Fallback title ///
-$pageTitle = "WeDoTech - De studie voor ICTers!";
-$metaDescription = "Bij WeDoTech leiden we creatieve studenten en programmeurs op doormiddel van code. ";
+if (!isset($parts[1]) || $parts[1] == '') {
+    /// Fallback title ///
+    $pageTitle = "WeDoTech - De studie voor ICTers!";
+    $metaDescription = "Bij WeDoTech leiden we creatieve studenten en programmeurs op doormiddel van code. ";
+}
+
 ?>
