6166eb8d8553b639bd3b2f5cac95158ee5fd2d59
Thijs <thijsvanwaveren@outlook.com>
2025-06-21 15:40:17 +0200

    Added basic html

--- views/stageBedrijven.php
+++ views/stageBedrijven.php
@@ -1,3 +1,4 @@
+
 <style>
 .collapsible {
   background-color: #777;
@@ -22,40 +23,59 @@
   background-color: #f1f1f1;
 }
 
-
 .pentagon {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	height: 0;
-	width: 200px;
-	border-top: 180px solid rgb(0, 66, 128);
-	border-left: 90px solid transparent;
-	border-right: 90px solid transparent; 
-	}
+  position: relative;
+  width: 200px;
+  height: 200px;
+  background-color: #555;
+  clip-path: polygon(50% 0%, 100% 38%, 81% 92%, 22% 92%, 0% 38%);
+  transform: rotate(60deg);
+}
 
-	.pentagon:after{
-	position: absolute;
-	content: '';
-	border-bottom: 180px solid rgb(0, 66, 128);
-	border-left: 180px solid transparent;
-	border-right: 180px solid transparent;
-	bottom: 200px;
-	left: -90px;
-	}
+.pentagon:before {
+  content = "";
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  width: 210px;
+  height: 210px;
+  background: red;
+  clip-path: polygon(50% 0%, 100% 38%, 81% 92%, 22% 92%, 0% 38%);
+  z-index: -1;
+  transform: rotate(60deg);
+}
 
 </style>
 
-<button type="button" class="collapsible">Verwachtingen</button>
-
-<div class="content">
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-    Ducimus laborum optio, eum dignissimos qui non natus architecto ratione ea nulla.
-    </p>
+<div>
+  home > Stagebedrijven
 </div>
 
-<div class="pentagon"></div>
+<div class="max-w-7xl mx-auto">
+  <button type="button" class="collapsible rounded-lg">Verwachtingen</button>
+  
+  <div class="content">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Ducimus laborum optio, eum dignissimos qui non natus architecto ratione ea nulla.
+      </p>
+  </div>
+</div>
+
+<div class="max-w-7xl mx-auto">
+  <div class="pentagon">
+    <div class="pentagon-outline"></div>
+    <img style="transform: rotate(-60deg);" src="./assets/images/femke_nijman.jpg" alt="Femke Nijman" draggable="false">
+  </div>
+  <div>
+    <h2 class="font-bold text-4xl">Vragen?</h2>
+    <p>Neem contact op met onze BPV-begeleider</p>
+
+    <a href="#">John-Doe@aventus.nl</a>
+    <a href="#">06-12345678</a>
+  </div>
+</div>
+
+
 
 <script>
     var collapsible = document.getElementsByClassName("collapsible");
