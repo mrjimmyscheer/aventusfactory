16fce6158b669ff677abd32ae16e63e121c6ef5e
Thijs <thijsvanwaveren@outlook.com>
2025-06-21 15:45:16 +0200

    Added basic html

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,27 @@
   <footer class="bg-gray-100 mt-16">
-    <div class="max-w-7xl mx-auto px-4 py-8 flex flex-col md:flex-row justify-between items-center text-center md:text-left">
-      <p class="text-gray-500">&copy; 2025 MyWebsite. All rights reserved.</p>
+    <div class="max-w-7xl mx-auto px-4 py-8 flex flex-col md:flex-row justify-between md:text-left">
+      <div class="mb-4">
+        <h3 class="font-bold text-2xl">Studenten</h3>
+        <ul>
+          <li>Toekomstige studenten</li>
+          <li>Informatie voor je stage</li>
+          <li>Bpv planning</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3 class="font-bold text-2xl">Stagebedrijven</h3>
+        <ul>
+          <li>Verwachtingen kennis & vaardigheden</li>
+          <li>Werkzaamheden</li>
+          <li>Leedoelen</li>
+          <li>Begeleiding</li>
+          <li>Logboek</li>
+          <li>Gesprekken</li>
+          <li>Onze ervaringen</li>
+        </ul>
+      </div>
+
       <div class="mt-4 md:mt-0 space-x-4">
         <a href="https://www.instagram.com/aventusmbo/" class="">Insta</a>
         <a href="https://www.facebook.com/aventus.school" class="">Facebook</a>
